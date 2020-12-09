fd0dd1f9e39162b9e9491fad4e6eb0da440b41fd
조성민 <p33a33@gmail.com>
2020-12-09 15:43:03 +0900

    [Micro modify] modify slideData type(add property 'id') for test
    * 글로벌 타입으로 사용하는 slideData에 id 속성이 추가됐습니다. id는 스트리밍, 상품 상세페이지 Dynamic Routing url로 사용될 예정입니다.
    * 햄버거 메뉴 버튼 크기 관련 수정사항이 있었습니다.

---
+++ pages/StreamingView/index.tsx
@@ -0,0 +1,29 @@
+import Layout from "../../components/Layout/Layout"
+// import Link from "next/link"
+
+const StreamingView = () => {
+    return (
+        <Layout title={"The Live | 현재 방송중인 목록"}>
+            <main> {/* streaming title, video player, chat */}
+                <section>
+                    <h1>
+
+                    </h1>
+                    <video>
+                    </video>
+
+                    <div> {/* buttonline, viewerCounter, streaming description*/}
+                    </div>
+                </section>
+
+                <aside> {/*chat area */}
+                </aside>
+            </main>
+            <section> {/* Linked item */}
+
+            </section>
+        </Layout>
+    )
+}
+
+export default StreamingView;
--- utils/getDatas.tsx
+++ utils/getDatas.tsx
@@ -6,12 +6,13 @@ export default function getDatas(carouselType: carouselType) {
     let dataDir: string = path.join(process.cwd(), `public/${carouselType}`)
     let dataFiles: Array<slideData> = fs.readdirSync(dataDir).map(file => {
         return {
+            id: file,
             imgSrc: (path.join(`/${carouselType}`, file)),
-            linkTo: "/StreamingList",
+            linkTo: `/StreamingView`,
             title: "test",
             viewerCount: Number((Number(Math.random().toFixed(4)) * 10000).toFixed(0)),
-            originalPrice: Number(Number(Math.random().toFixed(3)) * 3000000).toFixed(0),
-            currnetPrice: Number(Number(Math.random().toFixed(3)) * 1000000).toFixed(0)
+            originalPrice: Number((Number(Math.random().toFixed(3)) * 3000000).toFixed(0)),
+            currnetPrice: Number((Number(Math.random().toFixed(3)) * 1000000).toFixed(0))
         }
     })
     return dataFiles;
